38840d1699caf0f4f1b1100c646c02d6c1541968
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-29 11:08:07 -0500

    mini exercise Assertions

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void testCodeUp(){
+        assertEquals("CodeUp", "Codeup");
+    }
+
+    @Test
+    public void testArrayList(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages,moreLanguages);
+    }
+
+    @Test
+    public void testArrays(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers,otherNumbers);
+    }
+
+    @Test
+    public void testStringLang(){
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+        assertFalse(language.contains("J"));
+        assertTrue(language.contains("H"));
+    }
+
+
+}
---
+++ src/test/java/NewMainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NewMainTest {
+
+    @Test
+    public void testTwoisTwo(){
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void testAssertions(){
+        assertEquals("bob", "Bob");
+    }
+
+}
