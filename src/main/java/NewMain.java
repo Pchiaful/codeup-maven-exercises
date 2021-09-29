235ebdc580058f253de5db4fc5a7bb9e0e4c948d
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-29 09:42:31 -0500

    maven-exercises complete

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 public class NewMain {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string.");
+        String userInput = sc.nextLine();
+        System.out.println("You entered " + userInput + ".");
+        System.out.println(StringUtils.isNumeric(userInput));
+        System.out.println(StringUtils.swapCase(userInput));
+        System.out.println(StringUtils.reverse(userInput));
     }
 }
